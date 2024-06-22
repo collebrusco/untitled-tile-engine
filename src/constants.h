a6960f837d689f013dd73ab95efa86c0c80334cb
collebrusco <collebrusco.frank@gmail.com>
2024-06-22 12:03:44 -0500

    fix constant usage

--- src/constants.h
+++ src/constants.h
@@ -10,8 +10,8 @@
 
 #define WORLD_DIAMETER  (16)
 
-#define TILE_PIXELS     (32)
-#define TILE_PIXELS_F   (32.f)
+#define TILE_PIXELS     (16)
+#define TILE_PIXELS_F   (16.f)
 
 #define TILE_SPRITESHEET_DIM    (32)
 #define TILE_SPRITESHEET_DIM_F  (32.f)
--- src/rendering/RegionRenderer.cpp
+++ src/rendering/RegionRenderer.cpp
@@ -70,8 +70,8 @@ void RegionRenderer::init() {
 
 static void uvpushback(vector<vec2>& uvs, sprite_t img) {
     const double tsz = 1. / (32.);
-    double x = ((double)(img % TILE_PIXELS)) / TILE_PIXELS_F;
-    double y = ((double)(img / TILE_PIXELS)) / TILE_PIXELS_F;
+    double x = ((double)(img % TILE_SPRITESHEET_DIM)) / TILE_SPRITESHEET_DIM;
+    double y = ((double)(img / TILE_SPRITESHEET_DIM)) / TILE_SPRITESHEET_DIM;
     uvs.push_back({x    +0.00001,y+tsz-0.00001});
     uvs.push_back({x    +0.00001,y    +0.00001});
     uvs.push_back({x+tsz-0.00001,y    +0.00001});
