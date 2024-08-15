f10e1f935f4a72d653588e2badada88e8b6a9584
collebrusco <collebrusco.frank@gmail.com>
2024-08-15 17:57:16 -0500

    ugh

---
+++ src/data/NewAnimation.cpp
@@ -0,0 +1,4 @@
+#include "NewAnimation.h"
+
+
+
---
+++ src/data/NewAnimation.h
@@ -0,0 +1,15 @@
+/** 
+ * NewAnimation.h
+ * created 08/15/24 by frank collebrusco 
+ */
+#ifndef NEW_ANIMATION_H
+#define NEW_ANIMATION_H
+#include "ECS.h"
+#include <flgl/glm.h>
+
+struct Atlas {
+    
+};
+
+
+#endif /* NEW_ANIMATION_H */
